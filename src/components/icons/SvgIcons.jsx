7403874d5f137ce8ff7c35d152b609411d73c208
Enrique Maldonado <enmala@gmail.com>
2026-07-27 20:54:23 -0400

    refactor: update WaterDropIcon to stroke outline style matching set

--- src/components/icons/SvgIcons.jsx
+++ src/components/icons/SvgIcons.jsx
@@ -40,8 +40,8 @@ export function ClockIcon({ className = "w-4 h-4", ...props }) {
 
 export function WaterDropIcon({ className = "w-4 h-4", ...props }) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className={className} {...props}>
-      <path d="M12 2.69l5.66 5.66a8 8 0 1 1-11.31 0z" />
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className} {...props}>
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 2.25c-5.25 6-7.5 9.75-7.5 13.5a7.5 7.5 0 0 0 15 0c0-3.75-2.25-7.5-7.5-13.5Z" />
     </svg>
   );
 }
